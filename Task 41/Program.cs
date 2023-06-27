fe84501ab8a257377098bb5eda518417661d39fe
morpatims <kverteletski@mail.ru>
2023-06-27 15:52:39 +0300

    add tasks 41, 43

---
+++ Task 41/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Enter the numbers?: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int Count = 0;
+for (int i = 0; i < M; i++)
+{
+    Console.WriteLine($"Enter {i+1} number");
+    int m = Convert.ToInt32(Console.ReadLine());
+    if (m > 0)
+    {
+        Count = Count + 1;
+    }
+}
+Console.WriteLine($"Numbers greater than 0 {Count}");
---
+++ Task 43/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Enter  b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Intersection point of 2 lines X = {x:0.00} , Y = {y:0.00}");
